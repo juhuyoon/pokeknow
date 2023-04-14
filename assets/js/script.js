9a28aad12ab267cfe2bd77061dfeb600e22f8e58
jyoon3 <jyoon3@2u.com>
2023-04-14 14:17:49 -0400

    made it work after pulling out core feature as a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,29 @@ pokemonButton.addEventListener('click', function(event) {
     event.preventDefault();
     // console.log(pokemonInputBox.value);
     var userChoice = pokemonInputBox.value.toLowerCase();
-    fetch("https://pokeapi.co/api/v2/pokemon/" + userChoice)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            console.log(data.name);
-
-            var cardEl = document.createElement('div');
-            cardEl.setAttribute("class", "card col-md-3");
-            cardEl.setAttribute("style", "width: 20rem")
-            cardEl.textContent = data.name;
-            for(var i = 0; i < data.moves.length; i++) {
-                var moveNames = document.createElement('div');
-                moveNames.textContent = data.moves[i].move.name;
-                console.log(moveNames);
-                cardEl.appendChild(moveNames);
-            }
-            pokemonBox.appendChild(cardEl);
-        })
+    getPokemonData(userChoice);
     console.log("hi");
 })
 
+function getPokemonData(pokemonName) {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.name);
+
+        var cardEl = document.createElement('div');
+        cardEl.setAttribute("class", "card col-md-3");
+        cardEl.setAttribute("style", "width: 20rem")
+        cardEl.textContent = data.name;
+        for(var i = 0; i < data.moves.length; i++) {
+            var moveNames = document.createElement('div');
+            moveNames.textContent = data.moves[i].move.name;
+            console.log(moveNames);
+            cardEl.appendChild(moveNames);
+        }
+        pokemonBox.appendChild(cardEl);
+    })
+}
