ed662b3b7b8a579dadb3230631d2d1abb39431fa
jyoon3 <jyoon3@2u.com>
2023-04-14 14:07:56 -0400

    got the data to return and i can make the text appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
 var pokemonInputBox = document.getElementById('pokemon-finder');
-
 var pokemonButton = document.getElementById('search-btn');
-
-console.log(pokemonInputBox);
+var pokemonBox = document.getElementById('pokemon-box');
+// console.log(pokemonInputBox);
 
 pokemonButton.addEventListener('click', function(event) {
     event.preventDefault();
-    console.log(pokemonInputBox.value);
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        console.log(data.name);
-        console.log(data.abilities)
-    })
+    // console.log(pokemonInputBox.value);
+    var userChoice = pokemonInputBox.value.toLowerCase();
+    fetch("https://pokeapi.co/api/v2/pokemon/" + userChoice)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            console.log(data.name);
+            console.log(data.abilities)
+            var cardEl = document.createElement('div');
+            cardEl.textContent = data.name;
+            pokemonBox.appendChild(cardEl);
+        })
     console.log("hi");
 })
+
